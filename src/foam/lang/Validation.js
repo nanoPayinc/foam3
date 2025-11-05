3492b01d63cdd168e6cf093ed09e8de6a0365baa
Alexey Greer <alexey.greer@nanopay.net>
2025-11-05 11:49:48 -0500

    fix regex

--- src/foam/lang/Validation.js
+++ src/foam/lang/Validation.js
@@ -597,8 +597,8 @@ foam.CLASS({
   constants: [
     {
       name: 'ALPHA_CHAR_CHECK',
-      factory: () => /^[\d+-]*$/,
-      javaFactory: '  return "^[\\d+-]*$";'
+      factory: () => /^\+?\d+(?:-\d+)?$/,
+      javaFactory: '  return "^\\+?\\d+(?:-\\d+)?$";'
     }
   ],
 
