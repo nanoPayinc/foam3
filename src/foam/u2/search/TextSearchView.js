ae425d5e71b84ed1be07bbe7d0375100bd02e486
Chanmann Lim <chanmannlim@gmail.com>
2025-11-19 13:53:25 -0500

    Prioritize MQL search to improve performance
    - Do not mix with KEYWORD if search query is a valid mql

--- src/foam/u2/search/TextSearchView.js
+++ src/foam/u2/search/TextSearchView.js
@@ -153,13 +153,9 @@ foam.CLASS({
         }
         // TODO: dont think we ever use anything other than richSearch, maybe remove the boolean and only perform richSearch
         if ( this.richSearch ) {
-          if ( this.searchMode === this.SearchMode.FULL ) {
-            this.predicate = this.OR(
-              this.queryParser.parseString(value) || this.FALSE,
-              this.KEYWORD(value)
-            );
-          } else if ( this.searchMode === this.SearchMode.MQL ) {
-            this.predicate = this.queryParser.parseString(value) || this.FALSE;
+          var mql = this.queryParser.parseString(value);
+          if ( this.searchMode === this.SearchMode.MQL || mql ) {
+            this.predicate = mql || this.FALSE;
           } else {
             this.predicate = this.KEYWORD(value);
           }
