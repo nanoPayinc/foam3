a82bc68046c8a2c24f1f871a5d74e9cac393fe27
Alexey Greer <alexey.greer@nanopay.net>
2025-11-24 13:04:46 -0500

    remove group relationship

--- src/foam/lang/Relationships.js
+++ src/foam/lang/Relationships.js
@@ -6,10 +6,3 @@ foam.RELATIONSHIP({
    cardinality: '1:*'
  });
  
- foam.RELATIONSHIP({
-   sourceModel: 'foam.lang.Currency',
-   targetModel: 'foam.core.auth.Group',
-   forwardName: 'groups',
-   inverseName: 'currency',
-   cardinality: '1:*'
- });
