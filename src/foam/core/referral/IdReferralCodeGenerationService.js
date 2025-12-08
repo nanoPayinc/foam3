2cb9dcb4314b41305c86dee94184cd18d5af4597
Mykola <mykola@nanopay.net>
2025-12-08 15:15:09 -0500

    update headers

--- src/foam/core/referral/IdReferralCodeGenerationService.js
+++ src/foam/core/referral/IdReferralCodeGenerationService.js
@@ -1,18 +1,7 @@
 /**
- * NANOPAY CONFIDENTIAL
- *
- * [2025] nanopay Corporation
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of nanopay Corporation.
- * The intellectual and technical concepts contained
- * herein are proprietary to nanopay Corporation
- * and may be covered by Canadian and Foreign Patents, patents
- * in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from nanopay Corporation.
+ * @license
+ * Copyright 2025 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 foam.CLASS({
@@ -42,7 +31,7 @@ foam.CLASS({
         ReferralCode code = (ReferralCode) referralCodeDAO.find(
           AND(
             EQ(ReferralCode.REFERRER, userId),
-            EQ(ReferralCode.AUTO_GENERATED, true)
+            EQ(ReferralCode.ENABLED, true)
           )
         );
         return code.getId();
